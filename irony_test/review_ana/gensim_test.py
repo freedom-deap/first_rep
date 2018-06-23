3df1f117c5dda2e3b200117499f28f906bc28a3f
freed1245 <konryotada@gmail.com>
2018-06-23 20:16:34 +0900

    形態素解析

--- irony_test/review_ana/gensim_test.py
+++ irony_test/review_ana/gensim_test.py
@@ -6,3 +6,17 @@ Created on 2018/06/22
 '''
 
 import MeCab
+from time import sleep
+
+f = open('review_irony.txt',encoding='utf-8')
+
+m = MeCab.Tagger()
+i=0
+line = f.readline()
+while line:
+    print(m.parse(line))
+    #sleep(2)
+    print(i)
+    i=i+1
+    line = f.readline()
+print("finish!!")
